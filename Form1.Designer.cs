48f52eca1a5f74a9f84404883df6dd39301b697e
priyankadey1993 <140309817+priyankadey1993@users.noreply.github.com>
2023-12-04 11:34:52 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace gamedemp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.movebgtimer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.leftmovetimer = new System.Windows.Forms.Timer(this.components);
+            this.rightmovetimer = new System.Windows.Forms.Timer(this.components);
+            this.upmovetimer = new System.Windows.Forms.Timer(this.components);
+            this.downmovetimer = new System.Windows.Forms.Timer(this.components);
+            this.movemunitiontimer = new System.Windows.Forms.Timer(this.components);
+            this.enemytimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // movebgtimer
+            // 
+            this.movebgtimer.Enabled = true;
+            this.movebgtimer.Interval = 10;
+            this.movebgtimer.Tick += new System.EventHandler(this.movebgtimer_Tick);
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // Player
+            // 
+            this.Player.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.Player, "Player");
+            this.Player.Name = "Player";
+            this.Player.TabStop = false;
+            this.Player.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // leftmovetimer
+            // 
+            this.leftmovetimer.Interval = 5;
+            this.leftmovetimer.Tick += new System.EventHandler(this.leftmovetimer_Tick);
+            // 
+            // rightmovetimer
+            // 
+            this.rightmovetimer.Interval = 5;
+            this.rightmovetimer.Tick += new System.EventHandler(this.rightmovetimer_Tick);
+            // 
+            // upmovetimer
+            // 
+            this.upmovetimer.Interval = 5;
+            this.upmovetimer.Tick += new System.EventHandler(this.upmovetimer_Tick);
+            // 
+            // downmovetimer
+            // 
+            this.downmovetimer.Interval = 5;
+            this.downmovetimer.Tick += new System.EventHandler(this.downmovetimer_Tick);
+            // 
+            // movemunitiontimer
+            // 
+            this.movemunitiontimer.Enabled = true;
+            this.movemunitiontimer.Interval = 5;
+            this.movemunitiontimer.Tick += new System.EventHandler(this.movemunitiontimer_Tick);
+            // 
+            // enemytimer
+            // 
+            this.enemytimer.Enabled = true;
+            this.enemytimer.Interval = 20;
+            this.enemytimer.Tick += new System.EventHandler(this.enemytimer_Tick);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Blue;
+            this.Controls.Add(this.Player);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer movebgtimer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.Timer leftmovetimer;
+        private System.Windows.Forms.Timer rightmovetimer;
+        private System.Windows.Forms.Timer upmovetimer;
+        private System.Windows.Forms.Timer downmovetimer;
+        private System.Windows.Forms.Timer movemunitiontimer;
+        private System.Windows.Forms.Timer enemytimer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Schema;
+
+namespace gamedemp1
+{
+
+    public partial class Form1 : Form
+    {
+        PictureBox[] Stars;
+        Random rnd;
+        int backgroundspeed;
+        int playerspeed;
+        PictureBox[] munitions;
+        int Munitionspeed;
+        PictureBox[] enemies;
+        int enemiespeed;
+        
+        public Form1()
+        {
+            InitializeComponent();
+             
+        
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            backgroundspeed = 4;
+            playerspeed = 4;
+            Stars = new PictureBox[10];
+            rnd = new Random();
+            Munitionspeed = 20;
+            munitions = new PictureBox[3];
+            enemies = new PictureBox[5];
+            enemiespeed = 20;
+           /*
+            for(int i=0;i<enemies.Length;i++)
+            {
+                enemies[i] = new PictureBox();
+                enemies[i].Size = new Size(10, 10);
+                enemies[i].SizeMode = PictureBoxSizeMode.Zoom;
+                enemies[i].BorderStyle = BorderStyle.None; ;
+                this.Controls.Add(enemies[i]);
+            }
+           
+            enemies[0].Image = Image.FromFile(@"C:\\Users\\soura\\OneDrive\\Desktop\\Csharp_practice\\New folder\\star1.png");              
+            enemies[1].Image = Image.FromFile(@"C:\\Users\\soura\\OneDrive\\Desktop\\Csharp_practice\\New folder\\star2.png");
+            enemies[2].Image = Image.FromFile(@"C:\\Users\\soura\\OneDrive\\Desktop\\Csharp_practice\\New folder\\E2.png");
+            enemies[3].Image = Image.FromFile(@"C:\\Users\\soura\\OneDrive\\Desktop\\Csharp_practice\\New folder\\E3.png");
+            enemies[4].Image = Image.FromFile(@"C:\\Users\\soura\\OneDrive\\Desktop\\Csharp_practice\\New folder\\star2.png");
+            /*
+            enemies[5].Image = Image.FromFile(@"C:\\Users\\soura\\OneDrive\\Desktop\\Csharp_practice\\New folder\\munition.png");
+
+            enemies[6].Image = Image.FromFile(@"C:\\Users\\soura\\OneDrive\\Desktop\\Csharp_practice\\New folder\\munition.png");
+
+            enemies[7].Image = Image.FromFile(@"C:\\Users\\soura\\OneDrive\\Desktop\\Csharp_practice\\New folder\\munition.png");
+
+            enemies[8].Image = Image.FromFile(@"C:\\Users\\soura\\OneDrive\\Desktop\\Csharp_practice\\New folder\\munition.png");
+
+            enemies[9].Image = Image.FromFile(@"C:\\Users\\soura\\OneDrive\\Desktop\\Csharp_practice\\New folder\\munition.png");
+
+            enemies[10].Image = Image.FromFile(@"C:\\Users\\soura\\OneDrive\\Desktop\\Csharp_practice\\New folder\\munition.png");
+
+            enemies[0].Image = Image.FromFile(@"C:\\Users\\soura\\OneDrive\\Desktop\\Csharp_practice\\New folder\\munition.png");
+
+            enemies[0].Image = Image.FromFile(@"C:\\Users\\soura\\OneDrive\\Desktop\\Csharp_practice\\New folder\\munition.png");
+
+            enemies[0].Image = Image.FromFile(@"C:\\Users\\soura\\OneDrive\\Desktop\\Csharp_practice\\New folder\\munition.png");
+
+            enemies[0].Image = Image.FromFile(@"C:\\Users\\soura\\OneDrive\\Desktop\\Csharp_practice\\New folder\\munition.png");
+
+            enemies[0].Image = Image.FromFile(@"C:\\Users\\soura\\OneDrive\\Desktop\\Csharp_practice\\New folder\\munition.png");
+
+            enemies[0].Image = Image.FromFile(@"C:\\Users\\soura\\OneDrive\\Desktop\\Csharp_practice\\New folder\\munition.png");
+            */
+
+
+
+            Image munition = Image.FromFile(@"C:\\Users\\soura\\OneDrive\\Desktop\\Csharp_practice\\New folder\\munition.png");
+            for(int i=0;i< munitions.Length;i++)
+            {
+                munitions[i] = new PictureBox();
+                munitions[i].Size = new Size(8, 8);
+                munitions[i].Image= munition;
+                munitions[i].SizeMode = PictureBoxSizeMode.Zoom;
+                munitions[i].BorderStyle = BorderStyle.None; ;
+                this.Controls.Add(munitions[i]);
+            }
+
+            for(int i=0;i<Stars.Length;i++)
+            {
+                Stars[i]=new PictureBox();
+                Stars[i].BorderStyle = BorderStyle.None;
+                Stars[i].Location = new Point(rnd.Next(20,580),rnd.Next(-10,400));
+                if (i % 2 == 1)
+                {
+                    Stars[i].Size = new Size(2, 2);
+                    Stars[i].BackColor=Color.Wheat;
+
+                }
+                else
+                {
+                    Stars[i].Size = new Size(3,3);
+                    Stars[i].BackColor = Color.DarkGray;
+                }
+                this.Controls.Add(Stars[i]);
+            }
+
+
+        }
+
+        private void movebgtimer_Tick(object sender, EventArgs e)
+        {
+            for (int i=0;i<Stars.Length/2;i++)
+            {
+                Stars[i].Top += backgroundspeed;
+                if(Stars[i].Top >= this.Height)
+                {
+                    Stars[i].Top = -Stars[i].Height;
+                }
+
+            }
+
+            for (int i = Stars.Length / 2; i < Stars.Length; i++)
+            {
+                Stars[i].Top += backgroundspeed-2;
+                if (Stars[i].Top >= this.Height)
+                {
+                    Stars[i].Top = -Stars[i].Height;
+                }
+
+            }
+        }
+
+       private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void leftmovetimer_Tick(object sender, EventArgs e)
+        {
+            if (Player.Left > 10)
+                Player.Left -= playerspeed;
+        }
+
+        private void rightmovetimer_Tick(object sender, EventArgs e)
+        {
+            if (Player.Right < 500)
+                Player.Left += playerspeed;
+
+        }
+
+        private void upmovetimer_Tick(object sender, EventArgs e)
+        {
+            if (Player.Top > 10)
+                Player.Top -= playerspeed;
+
+        }
+
+        private void downmovetimer_Tick(object sender, EventArgs e)
+        {
+            if (Player.Bottom < 400)
+                Player.Top += playerspeed;
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Right)
+            {
+                rightmovetimer.Start();
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                leftmovetimer.Start();
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                upmovetimer.Start();
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                downmovetimer.Start();
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            rightmovetimer.Stop();
+            leftmovetimer.Stop();
+            upmovetimer.Stop();
+            downmovetimer.Stop();
+
+        }
+
+        private void movemunitiontimer_Tick(object sender, EventArgs e)
+        {
+            for(int i=0;i< munitions.Length;i++)
+            {
+                if (munitions[i].Top<400)
+                {
+                    munitions[i].Visible = true;
+                    munitions[i].Top -= Munitionspeed;
+                }
+                else
+                {
+                    munitions[i].Visible = false;
+                    munitions[i].Location = new Point(Player.Location.X +20, Player.Location.Y - i * 30);
+                }
+            }
+        }
+
+        private void enemytimer_Tick(object sender, EventArgs e)
+        {
+           /* for(int i=0;i<enemies.Length;i++)
+            {
+                if (enemies[i].Top < 400 )
+                {
+                    enemies[i].Visible = true;
+                    enemies[i].Top -= enemiespeed;
+                }
+                else
+                {
+                    enemies[i].Visible = false;
+                    enemies[i].Location = new Point(20, 20);
+                }
+            }*/
+        }
+
+        /*  private void timer2_Tick(object sender, EventArgs e)
+          {
+
+          }*/
+    }
+}
